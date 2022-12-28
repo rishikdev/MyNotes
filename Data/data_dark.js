adceaf0b5ee5295b1a6eaf5ea6312e06c08d7210
RishikDevTerminal <82426895+rishikdev@users.noreply.github.com>
2022-12-28 15:10:09 -0600

    UPDATED Website

---
+++ Data/data_dark.js
@@ -0,0 +1,72 @@
+export default
+[
+    {
+        device : "iPhone",
+        id : "iPhoneCarouselIndicator",
+        class : "iPhone",
+        carousel_elements : [
+            {
+                src : "https://github.com/rishikdev/Images/blob/main/My%20Notes/iPhone%2011%20Pro/Dark/Pinned.png",
+                alt : "Pinned Note",
+                header : "Pin Notes",
+                description : "Pin important notes to the top for quicker access!" 
+            },
+
+            {
+                src : "https://github.com/rishikdev/Images/blob/main/My%20Notes/iPhone%2011%20Pro/Dark/Filter%20Menu.png",
+                alt : "Filter & Sort Menu",
+                header : "Filter & Sort Notes",
+                description : "Filter and sort notes for better organisation!" 
+            },
+
+            {
+                src : "https://github.com/rishikdev/Images/blob/main/My%20Notes/iPhone%2011%20Pro/Dark/Context%20Menu.png",
+                alt : "Context Menu",
+                header : "Change Tags With Context Menu",
+                description : "In a hurry? Quickly change the tags from the list itself!" 
+            },
+
+            {
+                src : "https://github.com/rishikdev/Images/blob/main/My%20Notes/iPhone%2011%20Pro/Dark/Settings.png",
+                alt : "Settings Menu",
+                header : "Use Biometric Authentication",
+                description : "Lock your notes using Touch ID or Face ID for maximum security!" 
+            }
+        ]
+    },
+
+    {
+        device : "iPad",
+        id : "iPadCarouselIndicator",
+        class : "iPad",
+        carousel_elements : [
+            {
+                src : "https://github.com/rishikdev/Images/blob/main/My%20Notes/iPad%20Pro/Dark/Pinned.png",
+                alt : "Pinned Note",
+                header : "Pin Notes",
+                description : "Pin important notes to the top for quicker access!" 
+            },
+
+            {
+                src : "https://github.com/rishikdev/Images/blob/main/My%20Notes/iPad%20Pro/Dark/Filter%20Menu.png",
+                alt : "Filter & Sort Menu",
+                header : "Filter & Sort Notes",
+                description : "Filter and sort notes for better organisation!" 
+            },
+
+            {
+                src : "https://github.com/rishikdev/Images/blob/main/My%20Notes/iPad%20Pro/Dark/Context%20Menu.png",
+                alt : "Context Menu",
+                header : "Change Tags With Context Menu",
+                description : "In a hurry? Quickly change the tags from the list itself!" 
+            },
+
+            {
+                src : "https://github.com/rishikdev/Images/blob/main/My%20Notes/iPad%20Pro/Dark/Settings.png",
+                alt : "Settings Menu",
+                header : "Use Biometric Authentication",
+                description : "Lock your notes using Touch ID or Face ID for maximum security!" 
+            }
+        ]
+    }
+]
